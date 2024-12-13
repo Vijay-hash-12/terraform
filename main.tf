f53e6c0b48bf41f4c0a7506b2626d9986891e553
Vijay-hash-12 <vijayakumar.yadav@sonata-software.com>
2024-12-13 15:26:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,156 +112,21 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 
   computer_name  = "Vijaylinux"
-  
-  
   admin_username = "Vijay"  # Updated: Hardcoded the admin username as "Vijay"
-  
   
   admin_ssh_key {
     username   = "Vijay"
-    
-# 1. Configure the Azure provider
-provider "azurerm" {
-  features {}
-}
-
-# 2. Create a Resource Group
-resource "azurerm_resource_group" "v1" {
-  name     = "DevOps_CaseStudy_vijay"
-  location = "South India"  # Choose your preferred location
-}
-
-# 3. Create a Virtual Network (VNet)
-resource "azurerm_virtual_network" "v1" {
-  name                = "myvnet"
-  address_space       = ["10.0.0.0/16"]  # Define IP address range
-  location            = azurerm_resource_group.v1.location
-  resource_group_name = azurerm_resource_group.v1.name
-}
-
-# 4. Create a Subnet within the VNet
-resource "azurerm_subnet" "v1" {
-  name                 = "mysubnet"
-  resource_group_name  = azurerm_resource_group.v1.name
-  virtual_network_name = azurerm_virtual_network.v1.name
-  address_prefixes     = ["10.0.1.0/24"]  # Define IP address range for subnet
-}
-
-# 5. Create Public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.v1.location
-  resource_group_name = azurerm_resource_group.v1.name
-  allocation_method   = "Dynamic"
-}
-
-# 6. Create a Network Security Group (NSG) with an SSH rule (for Linux VM)
-resource "azurerm_network_security_group" "v1" {
-  name                = "mynsg"
-  location            = azurerm_resource_group.v1.location
-  resource_group_name = azurerm_resource_group.v1.name
-
-  security_rule {
-    name                       = "ssh"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"  # SSH port for Linux VM
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# 7. Create a Network Interface (NIC) and associate NSG
-resource "azurerm_network_interface" "v1" {
-  name                      = "myNIC"
-  location                  = azurerm_resource_group.v1.location
-  resource_group_name       = azurerm_resource_group.v1.name
-  ip_configuration {
-    name                          = "MyNIC_conf"
-    subnet_id                     = azurerm_subnet.v1.id  # Link subnet here
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id  # Link public IP
-  }
-}
-
-# 8. Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "v1" {
-  network_interface_id      = azurerm_network_interface.v1.id
-  network_security_group_id = azurerm_network_security_group.v1.id
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.v1.name
-  }
-
-  byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.v1.location
-  resource_group_name      = azurerm_resource_group.v1.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# 9. Create a Linux Virtual Machine with SSH key authentication
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "NodeV"  # VM name
-  resource_group_name = azurerm_resource_group.v1.name
-  location            = azurerm_resource_group.v1.location
-  network_interface_ids = [azurerm_network_interface.v1.id]  # Correct NIC reference
-  size                = "Standard_B1s"  # VM size
-
-  os_disk {
-    name                 = "mydisk"
-    caching              = "ReadWrite"  # Disk caching mode
-    storage_account_type = "Standard_LRS"  # Storage type for the OS disk
-  }
-
-  source_image_reference {
-    publisher = "Canonical"  # Publisher for Ubuntu images
-    offer     = "UbuntuServer"  # Offer for Ubuntu Linux OS
-    sku       = "18.04-LTS"  # Choose the Ubuntu version (e.g., 18.04-LTS)
-    version   = "latest"  # Use the latest version of the image
-  }
-
-  computer_name  = "Vijaylinux"
-  
-  
-  admin_username = "Vijay"  # Updated: Hardcoded the admin username as "Vijay"
-  
-  
-  admin_ssh_key {
-    username   = "Vijay"
-    
-    # Updated: Using file function to reference the public SSH key (make sure you have the file)
-   public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
   }
 
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
-# 9. Output the public IP of the VM
-output "public_ip" {
-  value = azurerm_public_ip.my_terraform_public_ip.ip_address
-}
+
+  depends_on = [azapi_resource_action.ssh_public_key_gen]
 }
 
-  }
-
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
-  }
-# 9. Output the public IP of the VM
+# Output the public IP of the VM
 output "public_ip" {
   value = azurerm_public_ip.my_terraform_public_ip.ip_address
-}
 }
