7f8b83455d4f19206687d0487b29c9d9c5b972a0
Vijay-hash-12 <vijayakumar.yadav@sonata-software.com>
2024-12-13 16:46:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,6 @@ provider "random" {
   # You can configure the provider here if needed
 }
 
-provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-}
+
 
 
